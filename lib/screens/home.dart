2b40d5072baf47b5745a67bb248847b08ce1ddd4
Pranav M <pranavm265@gmail.com>
2024-12-12 02:57:41 +0530

    modulation

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:parallax_rain/parallax_rain.dart';
-import 'package:typewritertext/typewritertext.dart';
+import 'package:prnvfolio_flutter/components/home_initial.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,45 +9,7 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
-  late AnimationController _arrowFadeController;
-  late Animation<double> _arrowFadeAnimation;
-  bool _showScrollText = false;
-
-  @override
-  void initState() {
-    super.initState();
-    
-    // Initialize the animation controller for the fade effect
-    _arrowFadeController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 1),
-    );
-
-    // Create a fade-in animation
-    _arrowFadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
-      CurvedAnimation(parent: _arrowFadeController, curve: Curves.easeIn),
-    );
-
-    // Trigger the fade-in after a delay to sync with the typewriter animation
-    Future.delayed(const Duration(milliseconds: 15000), () {
-      _arrowFadeController.forward();
-    });
-
-    // Show the "Scroll..." text after a delay
-    Future.delayed(const Duration(milliseconds: 16000), () {
-      setState(() {
-        _showScrollText = true;  // Update state to show the "Scroll..." text
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    _arrowFadeController.dispose();
-    super.dispose();
-  }
-
+class _HomeScreenState extends State<HomeScreen>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,60 +31,8 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
                 ],
               ),
             ),
-            Column(
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width - 80,
-                  height: MediaQuery.of(context).size.height - 80,
-                  margin: const EdgeInsets.all(50.00),
-                  color: Colors.black,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      TypeWriter.text(
-                        "Finally!?\nYou've Reached! 🤔\n\nLet me guide you through...",
-                        maintainSize: true,
-                        textAlign: TextAlign.center,
-                        style: GoogleFonts.specialElite(
-                          color: Colors.white,
-                          fontSize: 32,
-                        ),
-                        duration: const Duration(milliseconds: 200),
-                      ),
-                      const SizedBox(height: 130),
-                      FadeTransition(
-                        opacity: _arrowFadeAnimation,
-                        child: const Icon(
-                          Icons.keyboard_arrow_down,
-                          color: Colors.white,
-                          size: 50,
-                        ),
-                      ),
-                      const SizedBox(height: 30,),
-                      Container(
-                        child: _showScrollText ?
-                          TypeWriter.text(
-                            "Scroll...  ",
-                            maintainSize: true,
-                            textAlign: TextAlign.center,
-                            style: GoogleFonts.specialElite(
-                              color: Colors.white,
-                            ),
-                            duration: const Duration(milliseconds: 200),
-                          ) : Container(margin: const EdgeInsets.all(7) ,child: const Text("            "))
-                      )
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 100),
-                Container(
-                  width: MediaQuery.of(context).size.width - 80,
-                  height: MediaQuery.of(context).size.height - 80,
-                  margin: const EdgeInsets.all(50.00),
-                  color: Colors.black,
-                )
-              ],
-            ),
+            // Objective 1: Need to convert to ListView and build some kind of parallax on scroll lol
+            const HomeInitialContainer(),
           ],
         ),
       ),
